7513280833f88964d9dd963e650bea198bff7016
Innei <tukon479@gmail.com>
2021-06-17 19:32:58 +0800

    fix: omit non-value env

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,16 @@
  */
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { omitBy } from 'lodash'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  process.env = {
+    ...process.env,
+    ...omitBy(loadEnv(mode, process.cwd()), Boolean),
+  }
 
   return defineConfig({
     plugins: [vue(), tsconfigPaths(), visualizer({ open: false })],
