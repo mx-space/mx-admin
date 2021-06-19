15e36ccc26426a0d1c338114bd787ccd97b5a1cc
Innei <tukon479@gmail.com>
2021-06-19 15:35:41 +0800

    fix: n-spin

--- src/layouts/router-view.tsx
+++ src/layouts/router-view.tsx
@@ -1,6 +1,7 @@
 import { React } from '@vicons/fa'
 import { defineComponent } from '@vue/runtime-core'
-import NLoading from 'naive-ui/es/_internal/loading/src/loading'
+import { NSpin } from 'naive-ui'
+
 import { Suspense, VNode } from 'vue'
 import { RouteLocation, RouterView } from 'vue-router'
 const $RouterView = defineComponent({
@@ -17,10 +18,7 @@ const $RouterView = defineComponent({
                   fallback() {
                     return () => (
                       <div class="fixed left-1/2 top-1/2 transform text-primary-default -translate-y-1/2 -translate-x-1/2">
-                        <NLoading
-                          clsPrefix={'h-[50px] w-[50px] '}
-                          strokeWidth={14}
-                        />
+                        <NSpin strokeWidth={14} />
                       </div>
                     )
                   },
