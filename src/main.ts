afffa6ffa493ee19b407a94caeeee9ec4d67f965
Innei <tukon479@gmail.com>
2022-02-17 18:25:55 +0800

    fix: mocano worker

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 import 'reflect-metadata'
 import { bus } from 'utils/event-bus'
 import 'virtual:windi.css'
@@ -14,4 +19,22 @@ app.mount('#app')
 if (__DEV__) {
   window.app = app
   window.bus = bus
+}
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'json') {
+      return new jsonWorker()
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker()
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker()
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker()
+    }
+    return new editorWorker()
+  },
 }
