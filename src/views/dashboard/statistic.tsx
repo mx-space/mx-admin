c1db1d8f26bad04aa9a61a1be85a673a01ef55fb
Innei <i@innei.in>
2023-11-01 00:29:15 +0800

    fix: number format type error
    Signed-off-by: Innei <i@innei.in>

--- src/views/dashboard/statistic.tsx
+++ src/views/dashboard/statistic.tsx
@@ -4,21 +4,32 @@ import { defineComponent } from 'vue'
 export const Statistic = defineComponent({
   props: { label: String, value: [String, Number] },
   setup(props) {
-    return () => (
-      <Fragment>
-        {props.value === 'N/A' ? (
-          <NSpace vertical align="center" class="min-h-[4rem]">
-            <NSkeleton style={{ height: '.8rem', width: '5rem' }}></NSkeleton>
-            <NSkeleton style={{ height: '1.8rem', width: '3rem' }}></NSkeleton>
-          </NSpace>
-        ) : (
-          <NStatistic
-            label={props.label}
-            value={Intl.NumberFormat('en-us').format(props.value)}
-            tabularNums
-          ></NStatistic>
-        )}
-      </Fragment>
-    )
+    return () => {
+      const value = props.value
+      return (
+        <Fragment>
+          {props.value === 'N/A' ? (
+            <NSpace vertical align="center" class="min-h-[4rem]">
+              <NSkeleton style={{ height: '.8rem', width: '5rem' }}></NSkeleton>
+              <NSkeleton
+                style={{ height: '1.8rem', width: '3rem' }}
+              ></NSkeleton>
+            </NSpace>
+          ) : (
+            <NStatistic
+              label={props.label}
+              value={
+                typeof value === 'string'
+                  ? value
+                  : typeof value === 'undefined'
+                  ? ''
+                  : Intl.NumberFormat('en-us').format(value)
+              }
+              tabularNums
+            ></NStatistic>
+          )}
+        </Fragment>
+      )
+    }
   },
 })
