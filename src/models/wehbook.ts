7c67113aadf98ac677f627443894942b1e6c25ea
Innei <i@innei.in>
2023-12-24 21:00:38 +0800

    feat: webhook manage panel
    Signed-off-by: Innei <i@innei.in>

--- src/models/wehbook.ts
+++ src/models/wehbook.ts
@@ -4,6 +4,8 @@ export interface WebhookModel {
   enabled: boolean
   id: string
   secret: string
+
+  scope: number
 }
 
 type JSON = string
@@ -15,4 +17,16 @@ export declare class WebhookEventModel {
   success: boolean
   hookId: Ref<WebhookModel>
   status: number
+  id: string
+  timestamp: string
+}
+
+export const EventScope = {
+  TO_VISITOR: 1 << 0,
+  TO_ADMIN: 1 << 1,
+  TO_SYSTEM: 1 << 2,
+  // TO_VISITOR_ADMIN: (1 << 0) | (1 << 1),
+  // TO_SYSTEM_VISITOR: (1 << 0) | (1 << 2),
+  // TO_SYSTEM_ADMIN: (1 << 1) | (1 << 2),
+  ALL: (1 << 0) | (1 << 1) | (1 << 2),
 }
--- src/views/extra-features/webhook/index.tsx
+++ src/views/extra-features/webhook/index.tsx
@@ -10,17 +10,22 @@ import {
   NLayoutContent,
   NList,
   NListItem,
+  NPopconfirm,
+  NSpace,
   NSwitch,
   NThing,
   useDialog,
 } from 'naive-ui'
 import useSWRV from 'swrv'
-import type { WebhookModel } from '~/models/wehbook'
+import type { PaginateResult } from '@mx-space/api-client'
+import type { WebhookEventModel, WebhookModel } from '~/models/wehbook'
 import type { PropType } from 'vue'
 
 import { HeaderActionButton } from '~/components/button/rounded-button'
 import { PlusIcon } from '~/components/icons'
+import { JSONHighlight } from '~/components/json-highlight'
 import { ContentLayout } from '~/layouts/content'
+import { EventScope } from '~/models/wehbook'
 import { RESTManager } from '~/utils'
 
 const LIST_SWRV_KEY = 'webhook.list'
@@ -32,11 +37,12 @@ export default defineComponent({
     })
 
     const dialog = useDialog()
-    const presetCreateModal = () => {
+    const presetEditableModal = (formData?: WebhookModel) => {
       dialog.create({
         title: 'Create Webhook',
         content: () => (
           <EditWebhookForm
+            formData={formData}
             onSubmit={() => {
               mutate()
             }}
@@ -45,36 +51,99 @@ export default defineComponent({
         class: '!w-[800px] !max-w-[80vw]',
       })
     }
+
+    const handleShowDetail = (hookId: string) => {
+      dialog.create({
+        title: 'Webhook Dispatches',
+        content: () => <WebHookDispatches hookId={hookId} />,
+        class: '!w-[600px] !max-w-[80vw]',
+      })
+    }
     return () => (
       <ContentLayout
         actionsElement={
           <HeaderActionButton
             name="Add Webhook"
             icon={<PlusIcon />}
-            onClick={presetCreateModal}
+            onClick={() => presetEditableModal()}
           />
         }
       >
         <NList hoverable clickable>
           {data.value?.data.map((item) => {
             return (
-              <NListItem key={item.id}>
-                <NThing
-                  title={item.payloadUrl}
-                  description={item.events.join(',')}
-                >
+              <div
+                role="button"
+                tabindex={0}
+                onClick={() => {
+                  handleShowDetail(item.id)
+                }}
+              >
+                <NListItem key={item.id}>
                   {{
+                    default() {
+                      return (
+                        <NThing
+                          title={item.payloadUrl}
+                          description={
+                            item.events.slice(0, 3).join(', ') +
+                            (item.events.length > 3 ? '...' : '')
+                          }
+                        >
+                          {{
+                            avatar() {
+                              return (
+                                <div
+                                  class={[
+                                    'h-2 w-2 rounded-full',
+                                    item.enabled
+                                      ? 'bg-green-500'
+                                      : 'bg-gray-500',
+                                  ]}
+                                ></div>
+                              )
+                            },
+                          }}
+                        </NThing>
+                      )
+                    },
                     suffix() {
                       return (
                         <NButtonGroup>
-                          <NButton>编辑</NButton>
-                          <NButton>删除</NButton>
+                          <NButton
+                            round
+                            onClick={() => {
+                              presetEditableModal(item)
+                            }}
+                          >
+                            编辑
+                          </NButton>
+                          <NPopconfirm
+                            positiveText={'取消'}
+                            negativeText="删除"
+                            onNegativeClick={async () => {
+                              await RESTManager.api.webhooks(item.id).delete()
+                              mutate()
+                            }}
+                          >
+                            {{
+                              trigger: () => (
+                                <NButton round type="error" ghost>
+                                  移除
+                                </NButton>
+                              ),
+
+                              default: () => (
+                                <span class="max-w-48">确定要删除 ?</span>
+                              ),
+                            }}
+                          </NPopconfirm>
                         </NButtonGroup>
                       )
                     },
                   }}
-                </NThing>
-              </NListItem>
+                </NListItem>
+              </div>
             )
           })}
         </NList>
@@ -106,6 +175,7 @@ export const EditWebhookForm = defineComponent({
         ({
           events: [],
           enabled: true,
+          scope: EventScope.TO_SYSTEM,
         } as any),
     )
 
@@ -120,9 +190,15 @@ export const EditWebhookForm = defineComponent({
             ...reactiveFormData.value,
           },
         })
-        destroyAll()
-        props.onSubmit?.()
+      } else {
+        await RESTManager.api.webhooks(props.formData.id).patch({
+          data: {
+            ...reactiveFormData.value,
+          },
+        })
       }
+      destroyAll()
+      props.onSubmit?.()
     }
     return () => (
       <div class={'mt-5'}>
@@ -154,11 +230,28 @@ export const EditWebhookForm = defineComponent({
               class={'!h-[400px] !bg-transparent'}
             >
               <NGrid cols={2} xGap={12} yGap={12}>
+                <NGi>
+                  <NCheckbox
+                    checked={checkedEventsSet.value.has('all')}
+                    onUpdateChecked={(value) => {
+                      if (value) {
+                        reactiveFormData.value.events = ['all']
+                      } else {
+                        reactiveFormData.value.events = []
+                      }
+                    }}
+                  >
+                    ALL
+                  </NCheckbox>
+                </NGi>
                 {eventsRef.value?.data.map((item) => {
                   return (
                     <NGi>
                       <NCheckbox
-                        checked={checkedEventsSet.value.has(item)}
+                        checked={
+                          checkedEventsSet.value.has(item) ||
+                          checkedEventsSet.value.has('all')
+                        }
                         onUpdateChecked={(value) => {
                           if (value) {
                             reactiveFormData.value.events.push(item)
@@ -179,6 +272,31 @@ export const EditWebhookForm = defineComponent({
             </NLayoutContent>
           </NFormItem>
 
+          <NFormItem required label="Scope">
+            <NSpace wrap>
+              {Object.keys(EventScope).map((key) => {
+                const scope = EventScope[key]
+                const value = reactiveFormData.value.scope
+                return (
+                  <NCheckbox
+                    checked={
+                      (value & scope) === scope || value === EventScope.ALL
+                    }
+                    onUpdateChecked={(value) => {
+                      if (value) {
+                        reactiveFormData.value.scope |= EventScope[key]
+                      } else {
+                        reactiveFormData.value.scope &= ~EventScope[key]
+                      }
+                    }}
+                  >
+                    {key}
+                  </NCheckbox>
+                )
+              })}
+            </NSpace>
+          </NFormItem>
+
           <NFormItem required label="Enabled">
             <NSwitch
               onUpdateValue={(value) => {
@@ -197,3 +315,123 @@ export const EditWebhookForm = defineComponent({
     )
   },
 })
+
+const WebHookDispatches = defineComponent({
+  props: {
+    hookId: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const { data } = useSWRV(`webhook.events${props.hookId}`, async () => {
+      return await RESTManager.api
+        .webhooks(props.hookId)
+        .get<PaginateResult<WebhookEventModel>>({
+          params: { page: 1, size: 20 },
+        })
+        .then((d) => d.data)
+    })
+
+    const dialog = useDialog()
+
+    const handleDetailModal = (item: WebhookEventModel) => {
+      dialog.create({
+        title: 'Webhook Dispatch Detail',
+        class: '!w-[800px] !max-w-[80vw]',
+        content: () => (
+          <div class={'max-h-[80vh] overflow-auto'}>
+            <div class={'flex flex-col gap-4'}>
+              <NButton
+                class={'absolute right-4 top-4'}
+                onClick={() => {
+                  RESTManager.api.webhooks
+                    .redispatch(item.id)
+                    .post()
+                    .then(() => {
+                      message.success('Re-Dispatch Success')
+                    })
+                    .catch(() => {
+                      message.error('Re-Dispatch Failed')
+                    })
+                }}
+              >
+                Re-Dispatch
+              </NButton>
+              <p>
+                Status: {item.status} {item.success ? '✅' : '❌'}
+              </p>
+              <p>Header:</p>
+              <JSONHighlight
+                code={reintentJsonStringify(item.headers)}
+                class={'p-4'}
+              ></JSONHighlight>
+              <p>Payload:</p>
+              <JSONHighlight
+                code={reintentJsonStringify(item.payload)}
+                class={'p-4'}
+              ></JSONHighlight>
+              <p>Response:</p>
+              <JSONHighlight
+                code={reintentJsonStringify(item.response)}
+                class={'p-4'}
+              ></JSONHighlight>
+            </div>
+          </div>
+        ),
+      })
+    }
+
+    return () => (
+      <div>
+        <NList>
+          {data.value?.map((item) => {
+            return (
+              <NListItem key={item.id}>
+                <div class={'flex items-center space-x-4'}>
+                  <div
+                    class={[
+                      'h-2 w-2 rounded-full',
+                      item.success ? 'bg-green-500' : 'bg-red-500',
+                    ]}
+                  ></div>
+                  <div class={'w-[30px]'}>{item.status}</div>
+                  <span
+                    style={{
+                      flex: 2,
+                    }}
+                  >
+                    {new Date(item.timestamp).toLocaleString()}
+                  </span>
+                  <span
+                    style={{
+                      flex: 2,
+                    }}
+                  >
+                    {item.event}
+                  </span>
+                  <NButton
+                    onClick={() => handleDetailModal(item)}
+                    text
+                    type="primary"
+                    class={'justify-self-end'}
+                    style={{
+                      flex: 1,
+                    }}
+                  >
+                    Detail
+                  </NButton>
+                </div>
+              </NListItem>
+            )
+          })}
+        </NList>
+      </div>
+    )
+  },
+})
+
+const reintentJsonStringify = (obj: any) => {
+  return JSON.stringify(JSON.parse(obj), null, 2)
+}
