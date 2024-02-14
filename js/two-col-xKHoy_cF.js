f8adcaf699afd5944a95d65c6b95a98a7f05afee
Innei <Innei@users.noreply.github.com>
2024-02-14 12:37:15 +0000

    deploy: c604e29060d0fca92797919a2f4a5bef03d9d42f

---
+++ js/two-col-xKHoy_cF.js
@@ -0,0 +1,1 @@
+import{d as o,e as t,bw as a}from"./index-7UIvn_f6.js";const d=o({setup(r,{slots:e}){return()=>t(a,{cols:"36 1:12 1024:36 1600:36",xGap:24},{default:()=>[e?.default?.()]})}});export{d as T};
