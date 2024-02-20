6a8d2bbd16946538ebbe42d4329f6ba3b00dd635
Innei <Innei@users.noreply.github.com>
2024-02-20 14:03:28 +0000

    deploy: a488ca5053e90f4edb6649c255caa81d43c89076

---
+++ js/index-BkSjT5tb.js
@@ -0,0 +1,7 @@
+import{d,du as f,c as m,b as l,e,by as u,F as p,H as v,C as g,R as h,c3 as _}from"./index-Du_bmTZy.js";import{F as y,d as b}from"./index-mC_EWbQf.js";import{_ as S}from"./CheckCircleOutlined-Bp2FxjYL.js";import{T}from"./two-col-DoX1wQe7.js";const H=d({setup(){const s=f("debug-serverless",b),i=m(),t=l(),r=l(""),o=async()=>{try{const n=await h.api.debug.function.post({data:{function:s.value},errorHandler:a=>{r.value=`Error: ${a.data.message}`,i.error(a.data.message)}});_(()=>import("./index-Du_bmTZy.js").then(a=>a.e5),__vite__mapDeps([0,1])).then(a=>{a.editor.colorize(JSON.stringify(n.data,null,2),"typescript",{tabSize:2}).then(c=>{t.value.innerHTML=c}).catch(()=>{t.value.innerHTML=JSON.stringify(n,null,2)})})}catch{}};return()=>e(g,{actionsElement:e(p,null,[e(v,{icon:e(S,null,null),onClick:o},null)])},{default:()=>[e(T,null,{default:()=>[e(u,{span:"18"},{default:()=>[e("div",{class:"h-[80vh]"},[e(y,{value:s,onSave:o},null)])]}),e(u,{span:"18"},{default:()=>[e("pre",{class:"max-h-[calc(100vh-10rem)] overflow-auto !bg-transparent !bg-none",ref:t},[r.value])]})]})]})}});export{H as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["js/index-Du_bmTZy.js","assets/index-D1jYty6g.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ js/last-DXTTfs9a.js
@@ -0,0 +1,1 @@
+function l(n){var e=n==null?0:n.length;return e?n[e-1]:void 0}export{l};
